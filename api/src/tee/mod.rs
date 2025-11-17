8a71c40d92f9e69f327c08aec8808d0aae5bb590
Weikang Guo <guoweikang@kylinos.cn>
2025-11-17 14:31:02 +0800

    tee suport 2
    Signed-off-by: Weikang Guo <guoweikang@kylinos.cn>

--- api/src/tee/mod.rs
+++ api/src/tee/mod.rs
@@ -1,43 +1,29 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright (C) 2025 KylinSoft Co., Ltd. <https://www.kylinos.cn/>
-// See LICENSES for license details.
-//
-// This file has been created by KylinSoft on 2025.
-
-#![allow(non_camel_case_types, non_snake_case)]
-#![allow(unused_imports)]
-mod log;
-mod tee_fs;
-mod tee_obj;
-mod tee_pobj;
-mod tee_svc_cryp;
-mod user_access;
-mod tee_session;
-
-#[cfg(feature = "tee_test")]
-mod tee_unit_test;
-#[cfg(feature = "tee_test")]
-mod test;
-mod libutee;
-mod time;
-
-use log::*;
-use time::*;
-
-use axerrno::{AxError, AxResult};
 use axhal::uspace::UserContext;
 use syscalls::Sysno;
-#[cfg(feature = "tee_test")]
-use test::test_framework::{TestDescriptor, TestRunner};
-#[cfg(feature = "tee_test")]
-use test::test_framework_basic::TestResult;
+use tee_raw_sys::
 
-pub type TeeResult<T = ()> = Result<T, u32>;
+mod time;
 
-pub(crate) fn handle_tee_syscall(_sysno: Sysno, _uctx: &mut UserContext) -> AxResult<isize> {
+use time::*;
+
+pub(crate) fn handle_tee_syscall(uctx: &mut UserContext, sysno: Sysno) {
     // Handle TEE-specific syscalls here
-    match _sysno {
-        Sysno::tee_scn_log => sys_tee_scn_log(_uctx.arg0() as _, _uctx.arg1() as _),
-        _ => Err(AxError::Unsupported),
+    match sysno {
+        Sysno::tee_scn_get_time => {
+            let timeval_ptr = uctx.arg1() as *mut TeeTimeval;
+            let timezone_ptr = uctx.arg2() as *mut TeeTimezone;
+
+            match get_time(timeval_ptr, timezone_ptr) {
+                Ok(_) => {
+                    uctx.set_return_value(0); // Success
+                }
+                Err(err) => {
+                    uctx.set_return_value(err as usize); // Return error code
+                }
+            }
+        }
+        _ => {
+            panic!("Unknown TEE syscall number: {}", syscall_num);
+        }
     }
 }
