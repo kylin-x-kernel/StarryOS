fcb33861401e5f75846e3ccd6f21fb832c8d81de
Weikang Guo <guoweikang@kylinos.cn>
2025-11-19 17:31:40 +0800

    Add compile readme
    Signed-off-by: Weikang Guo <guoweikang@kylinos.cn>

--- api/src/tee/mod.rs
+++ api/src/tee/mod.rs
@@ -1,29 +1,12 @@
 use axhal::uspace::UserContext;
+use axerrno::{AxError, AxResult};
 use syscalls::Sysno;
-use tee_raw_sys::
 
 mod time;
 
 use time::*;
 
-pub(crate) fn handle_tee_syscall(uctx: &mut UserContext, sysno: Sysno) {
+pub(crate) fn handle_tee_syscall(_sysno: Sysno, _uctx: &mut UserContext) -> AxResult<isize>  {
     // Handle TEE-specific syscalls here
-    match sysno {
-        Sysno::tee_scn_get_time => {
-            let timeval_ptr = uctx.arg1() as *mut TeeTimeval;
-            let timezone_ptr = uctx.arg2() as *mut TeeTimezone;
-
-            match get_time(timeval_ptr, timezone_ptr) {
-                Ok(_) => {
-                    uctx.set_return_value(0); // Success
-                }
-                Err(err) => {
-                    uctx.set_return_value(err as usize); // Return error code
-                }
-            }
-        }
-        _ => {
-            panic!("Unknown TEE syscall number: {}", syscall_num);
-        }
-    }
+    Err(AxError::Unsupported)
 }
