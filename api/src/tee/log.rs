0105f9fd957c6fd53f2912525febd8aff978f91b
Weikang Guo <guoweikang@kylinos.cn>
2025-11-21 15:31:41 +0800

    now we support hello world TA
    Signed-off-by: Weikang Guo <guoweikang@kylinos.cn>

--- api/src/tee/log.rs
+++ api/src/tee/log.rs
@@ -4,36 +4,15 @@
 //
 // This file has been created by KylinSoft on 2025.
 
-#[cfg(feature = "tee_test")]
-use super::{
-    tee_obj::tee_obj, tee_unit_test::tee_test_unit, test::test_examples::tee_test_example,
-};
-use crate::mm::vm_load_string;
-use alloc::{boxed::Box, collections::VecDeque, string::String, sync::Arc, vec::Vec};
+
+use core::ffi::c_char;
 use axerrno::{AxError, AxResult};
-use core::{any::Any, ffi::c_char};
-use spin::RwLock;
-
-scope_local::scope_local! {
-    /// The current file descriptor table.
-    pub static SESSION_CTX: Arc<RwLock<String>> = Arc::default();
-}
-
+use crate::mm::vm_load_string;
 
 pub(crate) fn sys_tee_scn_log(buf: *const c_char, len: usize) -> AxResult<isize> {
     // Implementation for TEE log syscall we use info to output the log now
     info!("TEE log syscall invoked with len: {}", len);
     let message = vm_load_string(buf)?;
-
     info!("TEE Log: {}", message);
-
-    let mut ctx = SESSION_CTX.write();
-    ctx.push_str("abc");
-    info!("after push {}", *ctx);
-
-    #[cfg(feature = "tee_test")]
-    tee_test_example();
-    #[cfg(feature = "tee_test")]
-    tee_test_unit();
     Ok(0)
 }
--- api/src/tee/mod.rs
+++ api/src/tee/mod.rs
@@ -1,12 +1,27 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2025 KylinSoft Co., Ltd. <https://www.kylinos.cn/>
+// See LICENSES for license details.
+//
+// This file has been created by KylinSoft on 2025.
+
+
+mod time;
+mod log;
+
+use time::*;
+use log::*;
+
 use axhal::uspace::UserContext;
 use axerrno::{AxError, AxResult};
 use syscalls::Sysno;
 
-mod time;
-
-use time::*;
-
 pub(crate) fn handle_tee_syscall(_sysno: Sysno, _uctx: &mut UserContext) -> AxResult<isize>  {
     // Handle TEE-specific syscalls here
-    Err(AxError::Unsupported)
+    match _sysno {
+        Sysno::tee_scn_log => sys_tee_scn_log(
+            _uctx.arg0() as _,
+            _uctx.arg1() as _,
+        ),
+        _ => Err(AxError::Unsupported),
+    }
 }
