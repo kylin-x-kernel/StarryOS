45dce1c3bcb9299f72a8298272852ddec08f9e6b
于博 <yubo1@kylinos.cn>
2025-12-04 13:45:00 +0800

    add crypt obj functions
    Signed-off-by: 于博 <yubo1@kylinos.cn>

--- api/src/tee/user_access.rs
+++ api/src/tee/user_access.rs
@@ -25,37 +25,6 @@ pub(crate) fn copy_from_user(kaddr: &mut [u8], uaddr: &[u8], len: size_t) -> Tee
     }
 }
 
-pub fn copy_from_user_u64(s: &mut u64, user_s: &u64) -> TeeResult {
-    let s_bytes: &mut [u8] = unsafe {
-        core::slice::from_raw_parts_mut(s as *mut u64 as *mut u8, core::mem::size_of::<u64>())
-    };
-    let size_bytes: &[u8] = unsafe {
-        core::slice::from_raw_parts(
-            user_s as *const u64 as *const u8,
-            core::mem::size_of::<u64>(),
-        )
-    };
-    copy_from_user(s_bytes, size_bytes, core::mem::size_of::<u64>())?;
-
-    Ok(())
-}
-
-pub fn copy_to_user_u64(user_s: &mut u64, s: &u64) -> TeeResult {
-    let s_bytes: &[u8] = unsafe {
-        core::slice::from_raw_parts(s as *const u64 as *const u8, core::mem::size_of::<u64>())
-    };
-    let size_bytes: &mut [u8] = unsafe {
-        core::slice::from_raw_parts_mut(
-            user_s as *mut u64 as *mut u8,
-            core::mem::size_of::<u64>(),
-        )
-    };
-    copy_to_user(size_bytes, s_bytes, core::mem::size_of::<u64>())?;
-
-    Ok(())
-}
-
-
 pub(crate) fn copy_to_user(uaddr: &mut [u8], kaddr: &[u8], len: size_t) -> TeeResult {
     cfg_if::cfg_if! {
         if #[cfg(feature = "tee_test_mock_user_access")] {
@@ -89,11 +58,15 @@ pub mod tests_user_access {
         using TestResult;
 
         fn test_copy_from_user() {
-            let user_data: [u8; 5] = [1, 2, 3, 4, 5];
-            let mut kernel_data: [u8; 5] = [0; 5];
+            cfg_if::cfg_if! {
+            if #[cfg(feature = "tee_test_mock_user_access")] {
+                let user_data: [u8; 5] = [1, 2, 3, 4, 5];
+                let mut kernel_data: [u8; 5] = [0; 5];
 
-            copy_from_user(&mut kernel_data, &user_data, 5).unwrap();
-            assert_eq!(kernel_data, user_data);
+                copy_from_user(&mut kernel_data, &user_data, 5).unwrap();
+                assert_eq!(kernel_data, user_data);
+                }
+            }
         }
     }
 
@@ -101,28 +74,15 @@ pub mod tests_user_access {
         using TestResult;
 
         fn test_copy_to_user() {
-            let kernel_data: [u8; 5] = [10, 20, 30, 40, 50];
-            let mut user_data: [u8; 5] = [0; 5];
+            cfg_if::cfg_if! {
+            if #[cfg(feature = "tee_test_mock_user_access")] {
+                let kernel_data: [u8; 5] = [10, 20, 30, 40, 50];
+                let mut user_data: [u8; 5] = [0; 5];
 
-            copy_to_user(&mut user_data, &kernel_data, 5).unwrap();
-            assert_eq!(user_data, kernel_data);
-        }
-    }
-
-    test_fn! {
-        using TestResult;
-
-        fn test_copy_from_user_u64() {
-            let user_value: u64 = 0x1122334455667788;
-            let mut kernel_value: u64 = 0;
-
-            copy_from_user_u64(&mut kernel_value, &user_value).unwrap();
-            assert_eq!(kernel_value, user_value);
-
-            // test copy_to_user_u64
-            let mut user_value_out: u64 = 1;
-            copy_to_user_u64(&mut user_value_out, &kernel_value).unwrap();
-            assert_eq!(user_value_out, kernel_value);
+                copy_to_user(&mut user_data, &kernel_data, 5).unwrap();
+                assert_eq!(user_data, kernel_data);
+                }
+            }
         }
     }
 
@@ -131,6 +91,5 @@ pub mod tests_user_access {
         //------------------------
         test_copy_from_user,
         test_copy_to_user,
-        test_copy_from_user_u64,
     }
 }
