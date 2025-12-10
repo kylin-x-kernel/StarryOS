d017f832d68722eefb741493e8b8227bcfdf510c
于博 <yubo1@kylinos.cn>
2025-12-10 15:24:47 +0800

    add bignum.rs file
      - add libmbedtls mod
      - add rust-mbedtls crate
      - add bignum.rs files
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>

--- api/src/tee/tee_unit_test.rs
+++ api/src/tee/tee_unit_test.rs
@@ -7,6 +7,7 @@ pub fn tee_test_unit() {
     use super::tee_pobj::tests_tee_pobj::TEST_TEE_POBJ;
     use super::tee_svc_cryp::tests_tee_svc_cryp::TEST_TEE_SVC_CRYP;
     use super::user_access::tests_user_access::TEST_USER_ACCESS;
+    use super::libmbedtls::bignum::tests_tee_bignum::TEST_TEE_BIGNUM;
 
     let mut runner = TestRunner::new();
     run_tests!(
@@ -15,7 +16,8 @@ pub fn tee_test_unit() {
             TEST_TEE_POBJ,
             TEST_TEE_OBJ,
             TEST_TEE_SVC_CRYP,
-            TEST_USER_ACCESS
+            TEST_USER_ACCESS,
+            TEST_TEE_BIGNUM
         ]
     );
 }
