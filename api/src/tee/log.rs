02c9a252960473ec8eff901e836e9b5f450b2e22
WitcherTheWhite <1240749052@qq.com>
2025-12-10 15:12:23 +0800

    feat: add tee_scn_get_property_name_to_index syscall

--- api/src/tee/log.rs
+++ api/src/tee/log.rs
@@ -8,22 +8,25 @@
 use super::{
     tee_obj::tee_obj, tee_unit_test::tee_test_unit, test::test_examples::tee_test_example,
 };
-use crate::mm::vm_load_string;
+use crate::{mm::vm_load_string, tee::TeeResult};
 use alloc::{boxed::Box, collections::VecDeque, string::String, sync::Arc, vec::Vec};
 use axerrno::{AxError, AxResult};
 use core::{any::Any, ffi::c_char};
 use spin::RwLock;
+use tee_raw_sys::TEE_ERROR_BAD_PARAMETERS;
 
 scope_local::scope_local! {
     /// The current file descriptor table.
     pub static SESSION_CTX: Arc<RwLock<String>> = Arc::default();
 }
 
-
-pub(crate) fn sys_tee_scn_log(buf: *const c_char, len: usize) -> AxResult<isize> {
+pub(crate) fn sys_tee_scn_log(buf: *const c_char, len: usize) -> TeeResult {
     // Implementation for TEE log syscall we use info to output the log now
     info!("TEE log syscall invoked with len: {}", len);
-    let message = vm_load_string(buf)?;
+    let message = match vm_load_string(buf) {
+        Ok(s) => s,
+        Err(_) => return Err(TEE_ERROR_BAD_PARAMETERS),
+    };
 
     info!("TEE Log: {}", message);
 
@@ -35,5 +38,6 @@ pub(crate) fn sys_tee_scn_log(buf: *const c_char, len: usize) -> AxResult<isize>
     tee_test_example();
     #[cfg(feature = "tee_test")]
     tee_test_unit();
-    Ok(0)
+
+    Ok(())
 }
