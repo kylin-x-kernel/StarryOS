9d4f8f34e0cb87dec5380beb33da7db0fd4a0f43
WitcherTheWhite <1240749052@qq.com>
2025-12-16 14:40:33 +0800

    feat: add tee_scn_open_ta_session syscall [part 1]
    - Add in-progress funtion sys_tee_scn_open_ta_session
    - Add module tee_ta_manager to perform inter_ta ops
    - Add module protocol to define IPC data structures
    - Add module uuid to support UUID ops
    - Add bincode, uuid and hex crates

---
+++ api/src/tee/inter_ta.rs
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2025 KylinSoft Co., Ltd. <https://www.kylinos.cn/>
+// See LICENSES for license details.
+//
+// This file has been created by KylinSoft on 2025.
+
+use core::{
+    ffi::{c_uint, c_ulong},
+    ptr::addr_of,
+};
+
+use alloc::string::ToString;
+use tee_raw_sys::{TEE_Identity, TEE_LOGIN_TRUSTED_APP, TEE_UUID, utee_params};
+
+use crate::tee::{
+    TeeResult,
+    tee_session::{tee_session_ctx, with_tee_session_ctx},
+    tee_ta_manager::tee_ta_init_session,
+    user_access::copy_from_user,
+    uuid::Uuid,
+};
+
+pub(crate) fn sys_tee_scn_open_ta_session(
+    dest: *const TEE_UUID,
+    cancel_req_to: c_ulong,
+    usr_param: *mut utee_params,
+    ta_sees: *mut c_uint,
+    ret_orig: *mut c_uint,
+) -> TeeResult {
+    let uuid = TEE_UUID {
+        timeLow: 0,
+        timeMid: 0,
+        timeHiAndVersion: 0,
+        clockSeqAndNode: [0; 8],
+    };
+    let uuid_size = core::mem::size_of::<TEE_UUID>();
+    copy_from_user(
+        unsafe { core::slice::from_raw_parts_mut(addr_of!(uuid) as _, uuid_size) },
+        unsafe { core::slice::from_raw_parts(dest as _, uuid_size) },
+        uuid_size,
+    )?;
+
+    tee_ta_init_session(Uuid::from(uuid).to_string().as_str())?;
+
+    Ok(())
+}
---
+++ api/src/tee/protocol.rs
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2025 KylinSoft Co., Ltd. <https://www.kylinos.cn/>
+// See LICENSES for license details.
+//
+// This file has been created by KylinSoft on 2025.
+
+use alloc::{string::String, vec::Vec};
+use bincode::{Decode, Encode};
+
+#[derive(Encode, Decode)]
+pub enum TARequest {
+    Register { uuid: String },
+    OpenSession { uuid: String, params: Parameters },
+}
+
+#[derive(Encode, Decode)]
+pub enum CARequest {
+    OpenSession {
+        params: Parameters,
+    },
+    CloseSession {
+        session_id: u32,
+    },
+    Destroy,
+    InvokeCommand {
+        session_id: u32,
+        cmd_id: u32,
+        params: Parameters,
+    },
+}
+
+#[derive(Encode, Decode)]
+pub enum CAResponse {
+    OpenSession {
+        status: u32,
+        session_id: u32,
+    },
+    CloseSession {
+        status: u32,
+        session_id: u32,
+    },
+    Destroy {
+        status: u32,
+    },
+    InvokeCommand {
+        status: u32,
+        session_id: u32,
+        cmd_id: u32,
+        params: Parameters,
+    },
+}
+
+#[derive(Encode, Decode)]
+pub struct Parameters(pub Parameter, pub Parameter, pub Parameter, pub Parameter);
+
+impl Parameters {
+    pub fn default() -> Self {
+        Parameters(
+            Parameter::default(),
+            Parameter::default(),
+            Parameter::default(),
+            Parameter::default(),
+        )
+    }
+}
+
+#[derive(Encode, Decode)]
+pub struct Parameter {
+    pub raw: TEEParam,
+    pub param_type: ParamType,
+}
+
+impl Parameter {
+    pub fn default() -> Self {
+        Parameter {
+            raw: TEEParam {
+                data: Vec::new(),
+                value: Value { a: 0, b: 0 },
+            },
+            param_type: ParamType::None,
+        }
+    }
+}
+
+#[derive(Encode, Decode)]
+pub struct TEEParam {
+    pub data: Vec<u8>,
+    pub value: Value,
+}
+
+#[derive(Encode, Decode, Clone, Copy)]
+pub struct Value {
+    pub a: u32,
+    pub b: u32,
+}
+
+#[derive(Encode, Decode)]
+pub enum ParamType {
+    None = 0,
+    ValueInput = 1,
+    ValueOutput = 2,
+    ValueInout = 3,
+    MemrefInput = 5,
+    MemrefOutput = 6,
+    MemrefInout = 7,
+}
+
+impl From<u32> for ParamType {
+    fn from(value: u32) -> Self {
+        match value {
+            0 => ParamType::None,
+            1 => ParamType::ValueInput,
+            2 => ParamType::ValueOutput,
+            3 => ParamType::ValueInout,
+            5 => ParamType::MemrefInput,
+            6 => ParamType::MemrefOutput,
+            7 => ParamType::MemrefInout,
+            _ => ParamType::None,
+        }
+    }
+}
