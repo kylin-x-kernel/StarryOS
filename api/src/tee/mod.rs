ff73d7aff09c886245b7df7407cdaca99f9ceed3
zhangyoujing <zhangyoujing@kylinos.cn>
2025-12-29 10:04:48 +0800

    add tee_alg_get_class(gets the class of a given algorithm)

--- api/src/tee/mod.rs
+++ api/src/tee/mod.rs
@@ -34,6 +34,7 @@ mod tee_session;
 mod tee_svc_cryp;
 mod tee_svc_storage;
 mod tee_ta_manager;
+// mod ts_manager;
 #[cfg(feature = "tee_test")]
 mod tee_unit_test;
 #[cfg(feature = "tee_test")]
@@ -46,6 +47,9 @@ mod user_ta;
 mod utils;
 mod uuid;
 mod vm;
+mod tee_api_defines_extensions;
+pub use tee_api_defines_extensions::*;
+
 use core::arch::asm;
 
 use axerrno::{AxError, AxResult};
@@ -68,6 +72,7 @@ use crate::tee::{
     panic::sys_tee_scn_panic,
     property::{sys_tee_scn_get_property, sys_tee_scn_get_property_name_to_index},
     tee_time::{sys_tee_scn_wait,sys_tee_scn_get_time, sys_tee_scn_set_ta_time},
+    // tee_svc_cryp::sys_tee_scn_hash_init,
 };
 
 pub type TeeResult<T = ()> = Result<T, u32>;
@@ -134,6 +139,7 @@ pub(crate) fn handle_tee_syscall(_sysno: Sysno, _uctx: &mut UserContext) -> TeeR
             let teetime_ref = unsafe { &*teetime_ptr };
             sys_tee_scn_set_ta_time(teetime_ref)
         }
+        // Sysno::tee_scn_hash_init => sys_tee_scn_hash_init(_uctx.arg0() as _, _uctx.arg1() as _, _uctx.arg2() as _),
         _ => Err(TEE_ERROR_NOT_SUPPORTED),
     }
 }
