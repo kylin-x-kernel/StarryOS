28ee2137ce79d87920e62f5b54ec02572627b38f
zhangyoujing <zhangyoujing@kylinos.cn>
2025-12-29 10:43:12 +0800

    add sys_tee_scn_hash_init

--- api/src/tee/mod.rs
+++ api/src/tee/mod.rs
@@ -34,7 +34,7 @@ mod tee_session;
 mod tee_svc_cryp;
 mod tee_svc_storage;
 mod tee_ta_manager;
-// mod ts_manager;
+mod ts_manager;
 #[cfg(feature = "tee_test")]
 mod tee_unit_test;
 #[cfg(feature = "tee_test")]
@@ -73,6 +73,7 @@ use crate::tee::{
     property::{sys_tee_scn_get_property, sys_tee_scn_get_property_name_to_index},
     tee_time::{sys_tee_scn_wait,sys_tee_scn_get_time, sys_tee_scn_set_ta_time},
     // tee_svc_cryp::sys_tee_scn_hash_init,
+    tee_svc_cryp::sys_tee_scn_hash_init,
 };
 
 pub type TeeResult<T = ()> = Result<T, u32>;
@@ -140,6 +141,7 @@ pub(crate) fn handle_tee_syscall(_sysno: Sysno, _uctx: &mut UserContext) -> TeeR
             sys_tee_scn_set_ta_time(teetime_ref)
         }
         // Sysno::tee_scn_hash_init => sys_tee_scn_hash_init(_uctx.arg0() as _, _uctx.arg1() as _, _uctx.arg2() as _),
+        Sysno::tee_scn_hash_init => sys_tee_scn_hash_init(_uctx.arg0() as _, _uctx.arg1() as _, _uctx.arg2() as _),
         _ => Err(TEE_ERROR_NOT_SUPPORTED),
     }
 }
--- api/src/tee/tee_ta_manager.rs
+++ api/src/tee/tee_ta_manager.rs
@@ -162,3 +162,110 @@ pub fn tee_ta_get_session(handle: u32) -> TeeResult<SessionIdentity> {
         None => Err(TEE_ERROR_ITEM_NOT_FOUND),
     })
 }
+
+#[cfg(feature = "ta_gprof_support")]
+#[derive(Debug)]
+pub struct SampleBuf {
+    /// Size of samples array in uint16_t
+    pub nsamples: u32,
+    /// Passed from user mode
+    pub offset: u32,
+    /// Passed from user mode
+    pub scale: u32,
+    /// Number of samples taken
+    pub count: u32,
+    /// Sampling enabled?
+    pub enabled: bool,
+    /// Samples array
+    pub samples: Vec<u16>,
+    /// Total user CPU time for this session
+    pub usr: u64,
+    /// When this session last entered user mode
+    pub usr_entered: u64,
+    /// usr divided by freq is in seconds
+    pub freq: u32,
+}
+
+#[cfg(feature = "ta_gprof_support")]
+impl SampleBuf {
+    pub fn new(nsamples: u32, offset: u32, scale: u32, freq: u32) -> Self {
+        Self {
+            nsamples,
+            offset,
+            scale,
+            count: 0,
+            enabled: false,
+            samples: vec![0; nsamples as usize],
+            usr: 0,
+            usr_entered: 0,
+            freq,
+        }
+    }
+
+    /// Enable sampling
+    pub fn enable(&mut self) {
+        self.enabled = true;
+        self.count = 0;
+    }
+
+    /// Disable sampling
+    pub fn disable(&mut self) {
+        self.enabled = false;
+    }
+
+    /// Add a sample to the buffer
+    pub fn add_sample(&mut self, value: u16) -> TeeResult {
+        if !self.enabled {
+            // Sampling is not enabled
+            return Err(TEE_ERROR_NOT_SUPPORTED);
+        }
+
+        if self.count >= self.nsamples {
+            // Sample buffer is full
+            return Err(TEE_ERROR_OUT_OF_MEMORY);
+        }
+
+        let index = (self.count % self.nsamples) as usize;
+        self.samples[index] = value;
+        self.count += 1;
+
+        Ok(())
+    }
+
+    /// Reset the sample buffer
+    pub fn reset(&mut self) {
+        self.count = 0;
+        self.usr = 0;
+        self.usr_entered = 0;
+    }
+
+    /// Get the current number of samples
+    pub fn get_count(&self) -> u32 {
+        self.count
+    }
+
+    /// Get the total number of possible samples
+    pub fn get_capacity(&self) -> u32 {
+        self.nsamples
+    }
+
+    /// Get a reference to the samples array
+    pub fn get_samples(&self) -> &[u16] {
+        &self.samples
+    }
+
+    /// Set the time when the session last entered user mode
+    pub fn set_usr_entered(&mut self, time: u64) {
+        self.usr_entered = time;
+    }
+
+    /// Update the total user CPU time
+    pub fn update_usr_time(&mut self, additional_time: u64) {
+        self.usr += additional_time;
+    }
+
+    /// Get the total user CPU time
+    pub fn get_usr_time(&self) -> u64 {
+        self.usr
+    }
+}
