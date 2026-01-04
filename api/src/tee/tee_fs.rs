493edc75f0746642ab0ea333457f6c3954b8287f
于博 <yubo1@kylinos.cn>
2026-01-04 10:11:27 +0800

    tee: fs_dirfile add functions
      - tee_fs_dirfile_fileh_to_fname
      - tee_fs_dirfile_rename
      - read_dent
      - write_dent
      - tee_fs_dirfile_open
      - tee_fs_dirfile_find
      - tee_fs_dirfile_remove
      - tee_fs_dirfile_update_hash
      - tee_fs_dirfile_close
      - tee_fs_dirfile_commit_writes
      - tee_fs_dirfile_get_tmp
      - tee_fs_dirfile_get_next
    
    Signed-off-by: 于博 <yubo1@kylinos.cn>

--- api/src/tee/tee_fs.rs
+++ api/src/tee/tee_fs.rs
@@ -5,11 +5,25 @@
 // This file has been created by KylinSoft on 2025.
 
 use tee_raw_sys::TEE_OBJECT_ID_MAX_LEN;
+use super::tee_ree_fs::TeeFsFd;
+
+pub const TEE_FS_NAME_MAX: usize = 350;
+
+pub type TeeFileHandle = TeeFsFd;
 
 #[repr(C)]
+#[derive(Debug, Copy, Clone)]
 pub struct tee_fs_dirent {
     pub oid: [u8; TEE_OBJECT_ID_MAX_LEN as _],
     pub oid_len: u32,
 }
 
+impl Default for tee_fs_dirent {
+    fn default() -> Self {
+        Self {
+            oid: [0; TEE_OBJECT_ID_MAX_LEN as _],
+            oid_len: 0,
+        }
+    }
+}
 // pub type TeeFileHandle = TeeFsFd;
