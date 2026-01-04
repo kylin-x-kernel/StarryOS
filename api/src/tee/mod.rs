95b02506ba2b89ce9bd5cc02f551ef0e5c23b0f1
zhangyoujing <zhangyoujing@kylinos.cn>
2026-01-04 17:00:26 +0800

    add sys_tee_scn_hash_init

--- api/src/tee/mod.rs
+++ api/src/tee/mod.rs
@@ -36,7 +36,7 @@ mod tee_svc_storage;
 mod tee_ta_manager;
 mod tee_misc;
 mod ree_fs_rpc;
-// mod ts_manager;
+mod ts_manager;
 mod tee_fs_key_manager;
 mod huk_subkey;
 mod otp_stubs;
@@ -77,7 +77,7 @@ use crate::tee::{
     },
     panic::sys_tee_scn_panic,
     property::{sys_tee_scn_get_property, sys_tee_scn_get_property_name_to_index},
-    // tee_svc_cryp::sys_tee_scn_hash_init,
+    tee_svc_cryp::sys_tee_scn_hash_init,
     tee_time::{sys_tee_scn_wait,sys_tee_scn_get_time, sys_tee_scn_set_ta_time},
 };
 
@@ -151,7 +151,7 @@ pub(crate) fn handle_tee_syscall(_sysno: Sysno, _uctx: &mut UserContext) -> TeeR
             sys_tee_scn_set_ta_time(teetime_ref)
         }
 
-        // Sysno::tee_scn_hash_init => sys_tee_scn_hash_init(_uctx.arg0() as _, _uctx.arg1() as _, _uctx.arg2() as _),
+        Sysno::tee_scn_hash_init => sys_tee_scn_hash_init(_uctx.arg0() as _, _uctx.arg1() as _, _uctx.arg2() as _),
 
         _ => Err(TEE_ERROR_NOT_SUPPORTED),
     }
