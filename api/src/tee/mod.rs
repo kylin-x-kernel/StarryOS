a0136bc198acf3dcd42aa9fd2085c76cee53b819
WitcherTheWhite <1240749052@qq.com>
2026-01-15 13:55:52 +0800

    feat(tee): add test framework for tee

--- api/src/tee/mod.rs
+++ api/src/tee/mod.rs
@@ -2,11 +2,19 @@ use axhal::uspace::UserContext;
 use syscalls::Sysno;
 use tee_raw_sys::TEE_ERROR_NOT_SUPPORTED;
 
+#[cfg(feature = "tee_test")]
+use crate::tee::test::sys_tee_scn_test;
+
+#[cfg(feature = "tee_test")]
+mod test;
+
 pub type TeeResult<T = ()> = Result<T, u32>;
 
 pub fn handle_tee_syscall(sysno: Sysno, uctx: &mut UserContext) -> TeeResult {
     // Handle TEE-specific syscalls here
     match sysno {
+        #[cfg(feature = "tee_test")]
+        Sysno::tee_scn_test => sys_tee_scn_test(),
         _ => Err(TEE_ERROR_NOT_SUPPORTED),
     }
 }
---
+++ api/src/tee/test/test_unit_test.rs
@@ -0,0 +1,6 @@
+use crate::tee::test::test_framework::TestRunner;
+
+pub fn tee_test_unit() {
+    let mut runner = TestRunner::new();
+    // Here you would register and run your unit tests
+}
