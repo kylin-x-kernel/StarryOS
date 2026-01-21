d9286ef94a1508e8e9399b79ab09095561b97885
WitcherTheWhite <1240749052@qq.com>
2026-01-21 11:15:10 +0800

    update submodule

--- api/src/tee/test/mod.rs
+++ api/src/tee/test/mod.rs
@@ -1,15 +1,3 @@
 pub mod test_examples;
 pub mod test_framework;
 pub mod test_framework_basic;
-
-pub use test_framework::{TestDescriptor, TestRunner};
-
-use crate::tee::{TeeResult, tee_unit_test::tee_test_unit, test::test_examples::tee_test_example};
-
-#[cfg(feature = "tee_test")]
-pub(crate) fn sys_tee_scn_test() -> TeeResult {
-    tee_test_example();
-    tee_test_unit();
-
-    Ok(())
-}
